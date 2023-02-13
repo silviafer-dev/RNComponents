a04016b2cd5206e81462c96e119458dcb7b84211
Silvia Ferrai <silviafer@hotmail.it>
2023-02-13 12:54:24 +0100

    Use of Animated ValueXY

--- src/screens/Animation102Screen .tsx
+++ src/screens/Animation102Screen .tsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useRef } from 'react';
+import { StyleSheet, View, Animated, PanResponder } from 'react-native';
 
 export const Animation102Screen = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event(
+      [
+        null,
+        {
+          dx: pan.x,
+          dy: pan.y,
+        },
+      ],
+      { useNativeDriver: false },
+    ),
+    onPanResponderRelease: () => {
+      Animated.spring(pan, {
+        toValue: { x: 0, y: 0 },
+        useNativeDriver: false,
+      }).start();
+    },
+  });
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.purpleBox} />
+    <View style={styles.container}>
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[pan.getLayout(), styles.purpleBox]}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   purpleBox: {
     backgroundColor: 'red',
     width: 150,
