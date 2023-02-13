7b08516f288a327793361a0e4050f67edd2f8ca9
Silvia Ferrai <silviafer@hotmail.it>
2023-02-13 19:34:41 +0100

    Create reusable header

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 // import Icon from 'react-native-vector-icons/Ionicons';
-import { FlatList, Text, View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FlatList, View } from 'react-native';
 import { styles } from '../theme/appTheme';
 import { FlatListMenuItem } from '../components/FlatListMenuItem';
 import { menuItems } from '../data/menuItems';
+import { HeaderTitle } from '../components/HeaderTitle';
 
 export const HomeScreen = () => {
-  const { top } = useSafeAreaInsets();
-
-  const renderListHeader = () => {
-    return (
-      <View style={{ marginTop: top + 20, marginBottom: 20 }}>
-        <Text style={styles.title}>Opciones de Menu</Text>
-      </View>
-    );
-  };
-
   const itemSeparator = () => {
     return (
       <View style={{ borderBottomWidth: 1, opacity: 0.4, marginVertical: 8 }} />
@@ -29,7 +19,7 @@ export const HomeScreen = () => {
         data={menuItems}
         renderItem={({ item }) => <FlatListMenuItem menuItem={item} />}
         keyExtractor={item => item.name}
-        ListHeaderComponent={renderListHeader}
+        ListHeaderComponent={() => <HeaderTitle title="Opciones de menù" />}
         ItemSeparatorComponent={itemSeparator}
       />
     </View>
--- src/screens/SwitchScreen.tsx
+++ src/screens/SwitchScreen.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { View, Switch, Platform } from 'react-native';
+import { HeaderTitle } from '../components/HeaderTitle';
 
 export const SwitchScreen = () => {
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(!isEnabled);
 
   return (
-    <View style={{ marginTop: 100 }}>
+    <View style={{ marginHorizontal: 20 }}>
+      <HeaderTitle title="Switches" />
       <Switch
         trackColor={{ false: '#D9D9DB', true: '#5856D6' }}
         thumbColor={Platform.OS === 'android' ? '#8de4ff' : ''}
