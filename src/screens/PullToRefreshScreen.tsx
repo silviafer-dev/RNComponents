e8142b5501006c76c4f7934a378bf50c96df8ab1
Silvia Ferrai <silviaferdeveloper@gmail.com>
2023-02-23 14:24:08 +0100

    Customize Refresh Control

--- src/screens/PullToRefreshScreen.tsx
+++ src/screens/PullToRefreshScreen.tsx
@@ -2,24 +2,43 @@ import React, { useState } from 'react';
 import { RefreshControl, ScrollView, View } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const PullToRefreshScreen = () => {
-  const [refreshing, setRefreshing] = useState(false);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [data, setData] = useState<string>();
+
+  const { top } = useSafeAreaInsets(); // add space on top in IOS
 
   const onRefresh = () => {
-    setRefreshing(true);
+    setIsRefreshing(true);
     setTimeout(() => {
-      setRefreshing(false);
+      setIsRefreshing(false);
+      setData('Hola mundo');
       console.log('Terminado');
-    }, 1500);
+    }, 3500);
   };
   return (
     <ScrollView
+      style={{
+        marginTop: isRefreshing ? top : 0,
+      }}
       refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        <RefreshControl
+          refreshing={isRefreshing}
+          onRefresh={onRefresh}
+          progressViewOffset={150} //works only in android
+          progressBackgroundColor="lightblue" //background spinner android
+          colors={['white', 'red', 'orange']} // spinner colors android
+          style={{ backgroundColor: 'lightblue' }} //ios
+          tintColor="white" // ios
+          title="Refreshing" //ios
+          titleColor="black" //ios
+        />
       }>
       <View style={styles.globalMargin}>
         <HeaderTitle title="Pull to refresh" />
+        {data && <HeaderTitle title={data} />}
       </View>
     </ScrollView>
   );
