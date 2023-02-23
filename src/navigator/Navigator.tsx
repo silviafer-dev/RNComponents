13d8ec47f54559ca0a8c61aaec3341dc3d1d2eea
Silvia Ferrai <silviaferdeveloper@gmail.com>
2023-02-23 12:47:28 +0100

    Add RefreshControl component to pull to refresh

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -7,6 +7,7 @@ import { Animation101Screen } from '../screens/Animation101Screen';
 import { SwitchScreen } from '../screens/SwitchScreen';
 import { AlertScreen } from '../screens/AlertScreen';
 import { TextInputScreen } from '../screens/TextInputScreen';
+import { PullToRefreshScreen } from '../screens/PullToRefreshScreen';
 
 const Stack = createStackNavigator();
 
@@ -15,6 +16,9 @@ export function Navigator() {
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
+        cardStyle: {
+          backgroundColor: 'white',
+        },
       }}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
@@ -22,6 +26,10 @@ export function Navigator() {
       <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
       <Stack.Screen name="AlertScreen" component={AlertScreen} />
       <Stack.Screen name="TextInputScreen" component={TextInputScreen} />
+      <Stack.Screen
+        name="PullToRefreshScreen"
+        component={PullToRefreshScreen}
+      />
     </Stack.Navigator>
   );
 }
---
+++ src/screens/PullToRefreshScreen.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { RefreshControl, ScrollView, View } from 'react-native';
+import { HeaderTitle } from '../components/HeaderTitle';
+import { styles } from '../theme/appTheme';
+
+export const PullToRefreshScreen = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+      console.log('Terminado');
+    }, 1500);
+  };
+  return (
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
+      <View style={styles.globalMargin}>
+        <HeaderTitle title="Pull to refresh" />
+      </View>
+    </ScrollView>
+  );
+};
