fbbead82a851b573793127b1b73a710b18c4ea0f
Silvia Ferrai <silviaferdeveloper@gmail.com>
2023-03-05 17:04:29 +0100

    Theme change based on operating system

--- src/context/themeContext/ThemeContext.tsx
+++ src/context/themeContext/ThemeContext.tsx
@@ -1,16 +1,54 @@
-import React, { createContext, useReducer } from 'react';
-import { lightTheme, themeReducer, ThemeState } from './themeReducer';
+import React, { createContext, useEffect, useReducer } from 'react';
+import { useColorScheme, AppState, Appearance } from 'react-native';
+import {
+  lightTheme,
+  themeReducer,
+  ThemeState,
+  darkTheme,
+} from './themeReducer';
 
 interface ThemeContextProps {
   theme: ThemeState;
   setDarkTheme: () => void;
   setLightTheme: () => void;
 }
-
 export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({ children }: any) => {
-  const [theme, dispatch] = useReducer(themeReducer, lightTheme); // Todo: leer el tema global
+  const colorScheme = useColorScheme();
+
+  const [theme, dispatch] = useReducer(
+    themeReducer,
+    colorScheme === 'dark' ? darkTheme : lightTheme,
+  );
+
+  // const [theme, dispatch] = useReducer(
+  //   themeReducer,
+  //   Appearance.getColorScheme() === 'dark' ? darkTheme : lightTheme,
+  // );
+
+  //!cambio tema segun sistema operativo, antes funcionaba solo con IOS
+
+  useEffect(() => {
+    if (colorScheme === 'dark') {
+      setDarkTheme();
+    } else {
+      setLightTheme();
+    }
+  }, [colorScheme]);
+
+  //! Por si no funciona en Android hay esta posibilidad
+
+  // useEffect(() => {
+  //   AppState.addEventListener('change', status => {
+  //     if (status === 'active') {
+  //       Appearance.getColorScheme() === 'light'
+  //         ? setLightTheme()
+  //         : setDarkTheme();
+  //     }
+  //   });
+  // }, []);
+
   const setDarkTheme = () => {
     dispatch({ type: 'dark_theme' });
     console.log('setDark');
